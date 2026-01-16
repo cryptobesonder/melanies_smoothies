8270663833aa484f3f11ca06dc0e21883c993699
cryptobesonder <crypto.besonder.other@gmail.com>
2026-01-17 12:05:06 +1300

    Update streamlit_app.py
    Bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredient_list:
 
 #New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
